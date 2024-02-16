fcd4662ad07ece945c4bece7de6977e05655b4d5
Taha Ugurcan Ongel <dskylh@protonmail.com>
2024-02-16 18:15:20 +0300

    change daisyUI theme to 'cupcake'

--- app.vue
+++ app.vue
@@ -1,4 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const colorMode = useColorMode();
+colorMode.preference = "cupcake";
+</script>
 <template>
   <NuxtLayout>
     <NuxtPage />
