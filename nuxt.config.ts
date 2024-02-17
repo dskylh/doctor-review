c37c57fdb4367df980bca70cf7ef8ad4145e01a3
Taha Ugurcan Ongel <dskylh@protonmail.com>
2024-02-17 15:47:15 +0300

    update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	devtools: { enabled: true },
-	modules: [
-		"@nuxtjs/tailwindcss",
-		"@nuxtjs/color-mode",
-		"@nuxt/image",
-		"@pinia/nuxt",
-	],
-	colorMode: {
-		preference: "cupcake", // default theme
-		dataValue: "theme", // activate data-theme in <html> tag
-		classSuffix: "",
-	},
-	// imports: {
-	// 	dirs: ["stores"]
-	// },
-	pinia: {
+				devtools: { enabled: true },
+				modules: [
+				 "@nuxtjs/tailwindcss",
+				 "@nuxtjs/color-mode",
+				 "@nuxt/image",
+				 "@pinia/nuxt",
+				 "nuxt-icon",
+				],
+				colorMode: {
+								preference: "cupcake", // default theme
+								dataValue: "theme", // activate data-theme in <html> tag
+								classSuffix: "",
+				},
+				// imports: {
+				// 	dirs: ["stores"]
+				// },
+				pinia: {
     storesDirs: ['./stores/**', './custom-folder/stores/**'],
   },
 });
