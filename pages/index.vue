8be7c3fe470aad6ac6e4e39d19b08bb7943ebe1e
Taha Ugurcan Ongel <dskylh@protonmail.com>
2024-02-18 14:34:42 +0300

    update homepage placeholder

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,18 @@
 
 <template>
   <div class="flex flex-col">
-    <h1 class="text-3xl">Placeholder</h1>
+    <div class="hero min-h-screen bg-base-200">
+      <div class="hero-content text-center">
+        <div class="max-w-md">
+          <h1 class="text-5xl font-bold">Placeholder</h1>
+          <p class="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button class="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
